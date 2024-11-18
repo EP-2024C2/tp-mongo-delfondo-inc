2faa011bc8cd40f29879b3e43599d76e9c2ae6ff
Lu <lucauriel.tabaka@estudiantes.unahur.edu.ar>
2024-11-17 21:07:48 -0300

    Parte Final

--- src/controllers/componentes.controller.js
+++ src/controllers/componentes.controller.js
@@ -2,7 +2,8 @@ const { Componente, Producto } = require('../models')
 const controller = {}
 controller.componentes = Componente
 const mongoose = require("../db/mongo.db").mongoose;
-
+/*Esta busqueda valida los componentes en todos los productos entendiendo de que los componentes
+estan vinculados a los productos por lo que esta busqueda seria obsoleta*/
 const getAllParts = async (req, res)=>{
     const componentes = await Producto.find({},{"componentes": 1})
     res.status(200).json(componentes)
@@ -40,11 +41,12 @@ controller.updatePart = updatePart
 entendiendo de que este endPoint no deberia existir ya que no se deberian borrar componentes asociados a un producto.*/
 const deleteById = async (req,res)=>{
     const id = req.params.id
-    await Producto.updateOne({ 'componentes._id': id }, { $pull: { componentes: { _id: id } } } );
+    const prod = await Producto.findOne({'componentes._id': id})
+    await prod.componentes.find(c=> c.id===id).deleteOne()
+    await prod.save()
     return res.status(200).json(`El componente con id ${id} fue eliminado correctamente!`)
 }
 controller.deleteById = deleteById
-
 const getAllProductsMadeWithPart = async (req,res)=>{
     const id = req.params.id
     const componente = await Producto.findOne({'componentes._id': id}).select('-componentes')
